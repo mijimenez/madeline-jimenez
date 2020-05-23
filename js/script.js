182118cf553f8b41665a15d5c64b306cb09e270e
Madeline Jimenez <madelineijimenez@gmail.com>
2020-05-23 09:41:48 -0400

    Add code files

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// Animated mobile nav
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+
+  navLinks.forEach((link, index) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      burger.classList.toggle('toggle');
+      nav.classList.toggle('nav-active');
+      navLinks.forEach(setStyleLink)
+    })
+  });
+
+  burger.addEventListener('click', () => {
+    // Toggle Nav
+    nav.classList.toggle('nav-active');
+    // animate navlinks
+    navLinks.forEach(setStyleLink);
+    // burger animation
+    burger.classList.toggle('toggle');
+  });
+}
+
+function setStyleLink(el, index) {
+  if (el.style.animation) {
+    el.style.animation = ''
+  } else {
+    el.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 +0.5}s`;
+  }
+}
+navSlide();
+
+
+// Scroll to Anchor
+// @ https://perishablepress.com/vanilla-javascript-scroll-anchor/
+(function() {
+	scrollTo();
+})();
+
+function scrollTo() {
+	const links = document.querySelectorAll('.scroll');
+	links.forEach(each => (each.onclick = scrollAnchors));
+}
+
+function scrollAnchors(e, respond = null) {
+	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
+	e.preventDefault();
+	var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+	const targetAnchor = document.querySelector(targetID);
+	if (!targetAnchor) return;
+	const originalTop = distanceToTop(targetAnchor);
+	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+	const checkIfDone = setInterval(function() {
+		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+		if (distanceToTop(targetAnchor) === 0 || atBottom) {
+			targetAnchor.tabIndex = '-1';
+			targetAnchor.focus();
+			window.history.pushState('', '', targetID);
+			clearInterval(checkIfDone);
+		}
+	}, 100);
+}
