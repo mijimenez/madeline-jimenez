0fcaaedcd88a18f7ba6c032f68ca666ca882070c
Madeline Jimenez <madelineijimenez@gmail.com>
2020-05-23 15:04:44 -0400

    Fix distance from top scroll

--- js/script.js
+++ js/script.js
@@ -59,11 +59,10 @@ function scrollAnchors(e, respond = null) {
 	const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
 	e.preventDefault();
   var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
-  // console.log("Target ID: " + targetID);
   const targetAnchor = document.querySelector(targetID);
 	if (!targetAnchor) return;
 	const originalTop = distanceToTop(targetAnchor);
-	window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+	window.scrollBy({ top: (originalTop - 52), left: 0, behavior: 'smooth' });
 	const checkIfDone = setInterval(function() {
 		const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
 		if (distanceToTop(targetAnchor) === 0 || atBottom) {
